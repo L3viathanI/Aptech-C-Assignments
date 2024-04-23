08b89506480c623f87e656404b39e35b04ad5319
L3viathanI <tandumtoast@gmail.com>
2024-04-23 19:10:49 +0100

    Initial commit.

---
+++ mathFunctions/abs.c
@@ -0,0 +1,10 @@
+// cmath's abs example
+#include <stdio.h>     // std::cout
+#include <math.h>        // std::abs
+
+int main ()
+{
+  printf("abs (3.1416) = %.4f\n", abs(3.1416));
+  printf("abs (-10.6)  = %.4f\n", abs(-10.6));
+  return 0;
+}
---
+++ mathFunctions/nan.c
@@ -0,0 +1,10 @@
+/* nan example */
+#include <stdio.h>
+#include <math.h>
+
+
+int main() {
+    double param;
+    param = nan("");
+    printf("The value of param is %f", param);
+}
